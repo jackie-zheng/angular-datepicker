dfc9a68f463b7b755b1807b844a613c80684ed22
jackie <jackie.zheng@cardinfolink.com>
2017-12-13 16:52:00 +0800

    fix: min-date and max-date validateion is not cancel

--- app/scripts/input.js
+++ app/scripts/input.js
@@ -98,6 +98,11 @@ Module.directive('dateTime', ['$compile', '$document', '$filter', 'dateTimeConfi
         attrs.maxDate = date ? date.format() : date;
         maxValid = moment.isMoment(date);
       }
+      function isNotRequired(value) {
+        var required = $parse(attrs.ngRequired)(scope) || false;
+        //if a value is not required, and no value is specified pass the min check.
+        return (!required && !value);
+      }
 
       ngModel.$formatters.push(formatter);
       ngModel.$parsers.unshift(parser);
@@ -106,6 +111,9 @@ Module.directive('dateTime', ['$compile', '$document', '$filter', 'dateTimeConfi
         setMin(datePickerUtils.findParam(scope, attrs.minDate));
 
         ngModel.$validators.min = function (value) {
+          if (isNotRequired(value)) {
+            return true
+          }
           //If we don't have a min / max value, then any value is valid.
           return minValid ? moment.isMoment(value) && (minDate.isSame(value) || minDate.isBefore(value)) : true;
         };
@@ -115,6 +123,9 @@ Module.directive('dateTime', ['$compile', '$document', '$filter', 'dateTimeConfi
         setMax(datePickerUtils.findParam(scope, attrs.maxDate));
 
         ngModel.$validators.max = function (value) {
+          if (isNotRequired(value)) {
+            return true
+          }
           return maxValid ? moment.isMoment(value) && (maxDate.isSame(value) || maxDate.isAfter(value)) : true;
         };
       }
